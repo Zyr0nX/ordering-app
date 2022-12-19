93e7142178ad4ac61e4039decb4714794d34fe93
Hoang Duy Dang <duydh@smartosc.com>
2022-12-19 22:48:25 +0700

    Update header

--- src/components/ui/Navbar/Header.tsx
+++ src/components/ui/Navbar/Header.tsx
@@ -10,9 +10,10 @@ const Header = () => {
 >
   <Navbar.Brand href="https://flowbite.com/">
     <Image
-      src="https://flowbite.com/docs/images/logo.svg"
+      src=""
       className="mr-3 h-6 sm:h-9"
       alt="Flowbite Logo"
+      fill
     />
     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
       Flowbite
