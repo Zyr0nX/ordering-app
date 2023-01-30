7d597b807e71cbd97fc44fb1dcaf5e545e8a052f
Hoang Duy Dang <duydh@smartosc.com>
2023-01-30 15:33:05 +0700

    #1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 
 import CategoryBar from "../components/common/Category/CategoryBar";
 import ProductCard from "../components/common/Product/ProductCard";
+import IndexDropdown from "../components/notus/Dropdowns/IndexDropdown";
 import Header from "../components/ui/Header/Header";
 
 const Home: NextPage = () => {
@@ -19,6 +20,7 @@ const Home: NextPage = () => {
         </div>
         <CategoryBar />
         <ProductCard />
+        <IndexDropdown />
       </div>
     </>
   );
