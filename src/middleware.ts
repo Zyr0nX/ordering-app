d6d1e789bcc65daa55e1b8aeb101448d0a5b5e29
Hoang Duy Dang <duydh2000@gmail.com>
2023-02-27 23:21:27 +0700

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import { NextResponse } from "next/server";
 import { type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const country = request.geo?.country;
-  request.nextUrl.searchParams.set("country", country as string);
-  return NextResponse.rewrite(request.nextUrl);
+  const { nextUrl: url, geo } = request;
+  const country = geo?.country;
+  url.searchParams.set("country", country as string);
+  return NextResponse.rewrite(url);
 }
